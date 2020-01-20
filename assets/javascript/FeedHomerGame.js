3315a604af4c8c893823e410a80ebf6cb2c24d52
Tim-Shaffer <tcshaf24@ptd.net>
2020-01-20 13:34:51 -0500

    created feed homer js file and added some base variables and document ready function

---
+++ assets/javascript/FeedHomerGame.js
@@ -0,0 +1,25 @@
+// Global Variables
+var donutsNeeded  = 0;
+var donuts = [];
+var imgID = 0;
+var donutsFed = 0;
+var win = 0;
+var loss = 0;
+
+// Booleans
+isGameReady = false;
+
+// --------------------------------------------------------------------------------------
+// This function enacts when the html document has been loaded and is ready
+// --------------------------------------------------------------------------------------
+$(document).ready(function() {
+
+    $('body').on('click', '.images', function () { 
+        
+    });
+
+
+});
+// --------------------------------------------------------------------------------------
+// end of $(document).ready(function()
+// --------------------------------------------------------------------------------------
